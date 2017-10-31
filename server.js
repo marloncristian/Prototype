295e3490548c2625ceb000635dacf1919e8e49f1
Marlon Cristian Pereira <marloncristian@gmail.com>
2017-10-31 18:14:26 -0200

    adding port config

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+
+var port = process.env.PORT || 8080;
+
 app.use('/static', express.static('public'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(port, function () {
     console.log('Listening port: ' + server.address().port);
 });
  
